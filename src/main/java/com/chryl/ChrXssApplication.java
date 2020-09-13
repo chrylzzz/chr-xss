cf72408e418f36cde8421ea09c21a96396d3fa34
chryl <cyl5yy412@gmail.com>
2020-09-13 21:45:45 +0800

    Initial commit

---
+++ src/main/java/com/chryl/ChrXssApplication.java
@@ -0,0 +1,13 @@
+package com.chryl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChrXssApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChrXssApplication.class, args);
+	}
+
+}
